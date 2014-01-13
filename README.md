79e55387e85f3356e332d6bedf3cd1b7d797fe95
Liam Bowen <LiamBowen@gmail.com>
2014-01-13 18:00:05 -0500

    Updated readme for readme-driven development.  Started implementing things.

--- README.md
+++ README.md
@@ -1,13 +1,41 @@
-cookie_monster
-==============
+# cookie_monster
 
 Ruby Gem to use browser cookies
 
-Usage
------
+## Overview
 
 An instance of CookieMonster represents a browser's storage of
 cookies.  CookieMonster has a very rudimentary understanding of where
 the cookies are stored, and in what format.
 
-monster = CookieMonster.new(:browser)
+## Dependencies
+
+- sqlite3
+- ruby 1.9+
+
+## Example
+
+```ruby
+
+# Make a CookieMonster from your own browser's cookies
+monster = CookieMonster.new(browser: :chrome)
+# or perhaps it needs a bit of help to find it
+monster = CookieMonster.new(database: '/home/jhenson/.strange-dir/Cookies', browser: :chrome)
+# or from some YAML you made earlier
+monster = CookieMonster.new(yaml: '/home/jhenson/cookies.yaml')
+
+# Get all the cookies that could be read by/should be sent to www.lacecard.com
+cookie_header_str = monster.cookie_str('www.lacecard.com')
+
+# Export in the lingua franca of cookies (cookies.txt)
+cookies_txt_str = monster.netscape
+
+# Export to YAML
+cookies_yaml = monster.yaml
+cookies_yaml = monster.yaml('/home/jhenson/cookies.yaml')
+
+```
+
+## Wanted Contributions
+
+- Platform-specific DB locations (Windows / Mac / other Linux distros)
--- lib/cookie_monster.rb
+++ lib/cookie_monster.rb
@@ -1,13 +1,51 @@
+require 'yaml'
+
 
 class CookieMonster
-  def initialize(path, options={})
-    
+  def initialize(options={})
+    validate_options
+    make_source(options)
+  end
+
+  private
+
+  def validate_options
+    unless options[:browser]
+     raise CookieMonsterParameterError('No browser specified')
+    end
+  end
+
+  def make_source(options)
+    @source = case options[:browser]
+              when :firefox then FirefoxCookieSource.new
+              when :chrome then ChromiumCookieSource.new
+              when :chromium then ChromiumCookieSource.new
+              else nil
+              end
+    raise CookieMonster('') unless @source
+    if options[:source_db_path]
+      @source.db_path = options[:source_db_path]
+    else
+      @source.find_db
+    end
   end
 end
 
-class FirefoxCookieSource
-  
+class FirefoxCookieSource < CookieSource
+  def find_db
+    raise CookieMonsterException('Could not find Firefox cookie database')
+  end
 end
 
-class ChromiumCookieSource
+class ChromiumCookieSource < CookieSource
+  def find_db
+    raise CookieMonsterException('Could not find Chromium cookie database')
+  end
 end
+
+class CookieSource
+  attr_accessor :db_path
+end
+
+class CookieMonsterError < StandardError; end
+class CookieMonsterParameterError < ArgumentError; end
